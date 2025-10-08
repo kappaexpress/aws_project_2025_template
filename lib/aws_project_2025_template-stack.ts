6282a8f094723d8c63a864d7fbb8c107958f498f
kappaexpress <46478065+kappaexpress@users.noreply.github.com>
2025-10-08 00:15:42 +0000

    Add HTML files for 404 error page and index page; update S3 bucket configuration for static website hosting

--- lib/aws_project_2025_template-stack.ts
+++ lib/aws_project_2025_template-stack.ts
@@ -2,51 +2,31 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class AwsProject2025TemplateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // S3バケットを作成
+    // S3バケットを作成（静的ウェブサイトホスティング有効化）
     const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
       websiteIndexDocument: 'index.html',
       websiteErrorDocument: 'error.html',
-      publicReadAccess: false,
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS_ONLY,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    // CloudFront ディストリビューション
-    const distribution = new cloudfront.Distribution(this, 'Distribution', {
-      defaultBehavior: {
-        origin: origins.S3BucketOrigin.withOriginAccessControl(websiteBucket),
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-      },
-      defaultRootObject: 'index.html',
-      errorResponses: [
-        {
-          httpStatus: 404,
-          responseHttpStatus: 404,
-          responsePagePath: '/error.html',
-        },
-      ],
-    });
-
     // HTMLファイルをS3にデプロイ
     new s3deploy.BucketDeployment(this, 'DeployWebsite', {
-      sources: [s3deploy.Source.asset('./website')],
+      sources: [s3deploy.Source.asset('./frontend')],
       destinationBucket: websiteBucket,
-      distribution,
-      distributionPaths: ['/*'],
     });
 
-    // CloudFront URLを出力
-    new cdk.CfnOutput(this, 'DistributionDomainName', {
-      value: distribution.distributionDomainName,
-      description: 'CloudFront Distribution URL',
+    // S3ウェブサイトURLを出力
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: 'S3 Website URL',
     });
 
     // S3バケット名を出力
